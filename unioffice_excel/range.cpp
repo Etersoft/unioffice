83bc40d82379d2fba10ceee66d6ca94cd523a01a
Ivan Sinitsin <ivan@etersoft.ru>
2009-10-11 11:55:56 +0400

    Add implementation of AddRef, Release, QueryInterface  functions ( CRange )

--- unioffice_excel/range.cpp
+++ unioffice_excel/range.cpp
@@ -25,22 +25,64 @@
 
 
        // IUnknown
-       HRESULT STDMETHODCALLTYPE CRange::CRange::QueryInterface(const IID& iid, void** ppv)
+HRESULT STDMETHODCALLTYPE CRange::CRange::QueryInterface(const IID& iid, void** ppv)
 {
-    TRACE_NOTIMPL;
-    return E_NOTIMPL; 		
+    *ppv = NULL;    
+        
+    if ( iid == IID_IUnknown ) {
+        TRACE("IUnknown \n");
+        *ppv = static_cast<IUnknown*>(static_cast<IRange*>(this));
+    }
+        
+    if ( iid == IID_IDispatch ) {
+        TRACE("IDispatch \n");
+        *ppv = static_cast<IDispatch*>(static_cast<IRange*>(this));
+    }     
+    
+    if ( iid == IID_IRange) {
+        TRACE("IRange\n");
+        *ppv = static_cast<IRange*>(this);
+    } 
+    
+    if ( iid == DIID_Range ) {
+        TRACE("Range \n");
+        *ppv = static_cast<Range*>(this);
+    }   
+      
+    if ( *ppv != NULL ) 
+    {
+        reinterpret_cast<IUnknown*>(*ppv)->AddRef();
+         
+        return S_OK;
+    } else
+    {    
+        WCHAR str_clsid[39];
+         
+        StringFromGUID2( iid, str_clsid, 39);
+        WTRACE(L"(%s) not supported \n", str_clsid);
+        
+        return E_NOINTERFACE;                          
+    } 		
 }
         
-       ULONG STDMETHODCALLTYPE CRange::AddRef()
+ULONG STDMETHODCALLTYPE CRange::AddRef( )
 {
-    TRACE_NOTIMPL;
-    return E_NOTIMPL; 		
+      TRACE( " ref = %i \n", m_cRef );
+      
+      return InterlockedIncrement(&m_cRef); 		
 }
         
-       ULONG STDMETHODCALLTYPE CRange::Release()
+ULONG STDMETHODCALLTYPE CRange::Release( )
 {
-    TRACE_NOTIMPL;
-    return E_NOTIMPL; 		
+      TRACE( " ref = %i \n", m_cRef );
+      
+      if (InterlockedDecrement(&m_cRef) == 0)
+      {
+              delete this;
+              return 0;
+      }
+      
+      return m_cRef; 		
 }
         
        
