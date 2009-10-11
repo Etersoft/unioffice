55ebf70751d47aaa93e5b582c452a5d143e39aed
Ivan Sinitsin <ivan@etersoft.ru>
2009-10-11 11:49:47 +0400

    Add implementation of method Init (CRange)

--- unioffice_excel/range.cpp
+++ unioffice_excel/range.cpp
@@ -2060,8 +2060,34 @@
  
 HRESULT CRange::Init( )
 {
-    TRACE_NOTIMPL;
-    return E_NOTIMPL; 		
+     HRESULT hr = S_OK;   
+      
+     if (m_pITypeInfo == NULL)
+     {
+        ITypeLib* pITypeLib = NULL;
+        hr = LoadRegTypeLib(LIBID_Office,
+                                1, 0, // Номера версии
+                                0x00,
+                                &pITypeLib); 
+        
+       if (FAILED(hr))
+       {
+           ERR( " Typelib not register \n" );
+           return hr;
+       } 
+        
+       // Получить информацию типа для интерфейса объекта
+       hr = pITypeLib->GetTypeInfoOfGuid(IID_IRange, &m_pITypeInfo);
+       pITypeLib->Release();
+       if (FAILED(hr))
+       {
+          ERR(" GetTypeInfoOfGuid \n ");
+          return hr;
+       }
+       
+     }
+
+     return hr; 		
 }
          
 HRESULT CRange::Put_Application( void* )
