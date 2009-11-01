0b82068758914ecf79af85b1fa168278c3072a1e
Ivan Sinitsin <ivan@etersoft.ru>
2009-11-01 17:37:01 +0300

    Add description of XNameAccess interface for wrappers

---
+++ OOWrappers/com/sun/star/container/x_name_access.cpp
@@ -0,0 +1,67 @@
+/*
+ * source file - XNameAccess
+ *
+ * Copyright (C) 2009 Sinitsin Ivan (Etersoft) <ivan@etersoft.ru>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
+ */
+
+#include "x_name_access.h"
+   
+com::sun::star::container::XNameAccess::XNameAccess( ):XElementAccess()
+{                                                                       
+}
+
+com::sun::star::container::XNameAccess::~XNameAccess( )
+{              							
+} 
+
+com::sun::star::uno::XBase com::sun::star::container::XNameAccess::getByName( BSTR name )
+{
+    TRACE_IN; 
+    HRESULT hr;
+    VARIANT res, var_name;
+	com::sun::star::uno::XBase ret_val;    
+    
+    VariantInit( &res );
+    VariantInit( &var_name );
+    
+    if ( IsNull() )
+    {
+        ERR( " m_pd_wrapper is NULL \n" );
+        return ( ret_val );     
+    }
+    
+    V_VT( &res ) = VT_BSTR;
+    V_BSTR( &var_name ) = SysAllocString( name );
+    
+    hr = AutoWrap (DISPATCH_METHOD, &res, m_pd_wrapper, L"getByName", 1, var_name);
+    if ( FAILED( hr ) )
+    {
+        ERR( " Call getByName \n" );
+    } else
+    {
+        ret_val.Init( V_DISPATCH( &res ) );      
+    }
+ 
+    VariantClear( &res );
+    VariantClear( &var_name );
+ 
+    TRACE_OUT;
+    return ( ret_val ); 						   
+}
+
+
+  
---
+++ OOWrappers/com/sun/star/container/x_name_access.h
@@ -0,0 +1,53 @@
+/*
+ * header file - XNameAccess
+ *
+ * Copyright (C) 2009 Sinitsin Ivan (Etersoft) <ivan@etersoft.ru>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
+ */
+
+#ifndef __UNIOFFICE_X_NAME_ACCESS_H__
+#define __UNIOFFICE_X_NAME_ACCESS_H__
+
+#include "x_element_access.h"
+
+namespace com
+{
+    namespace sun
+    {
+        namespace star
+        {
+		    namespace container 
+			{ 
+ 		  
+			class XNameAccess: public virtual XElementAccess
+			{
+			public:
+       
+			  XNameAccess( );
+			  virtual ~XNameAccess( );    
+			  
+			  com::sun::star::uno::XBase getByName( BSTR name ); 
+         
+			protected:            
+								  
+			};
+
+            } // namespace container
+        } // namespace star
+    } // namespace sun
+} // namespace com
+
+#endif // __UNIOFFICE_X_NAME_ACCESS_H__
