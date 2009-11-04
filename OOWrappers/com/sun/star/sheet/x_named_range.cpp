09753337a79ac180e8d86471aec02f763c47e86c
Ivan Sinitsin <ivan@etersoft.ru>
2009-11-04 14:01:46 +0300

    Add full implementation of getReferencePosition (XNamedRange)

--- OOWrappers/com/sun/star/sheet/x_named_range.cpp
+++ OOWrappers/com/sun/star/sheet/x_named_range.cpp
@@ -92,7 +92,32 @@ HRESULT com::sun::star::sheet::XNamedRange::setContent( BSTR _content)
 			  
 com::sun::star::table::SCellAddress com::sun::star::sheet::XNamedRange::getReferencePosition()
 {
- 	 TRACE_NOTIMPL;	 									
+    TRACE_IN; 
+    HRESULT hr;
+    VARIANT res;
+	com::sun::star::table::SCellAddress ret_val;    
+    
+    VariantInit( &res );
+    
+    if ( IsNull() )
+    {
+        ERR( " m_pd_wrapper is NULL \n" );
+        return ( ret_val );     
+    }
+    
+    hr = AutoWrap (DISPATCH_METHOD, &res, m_pd_wrapper, L"getReferencePosition", 0);
+    if ( FAILED( hr ) )
+    {
+        ERR( " Call getReferencePosition \n" );
+    } else
+    {
+        ret_val.Init( V_DISPATCH( &res ) );      
+    }
+    
+    VariantClear( &res );
+ 
+    TRACE_OUT;
+    return ( ret_val ); 									
 }
 
 HRESULT com::sun::star::sheet::XNamedRange::setReferencePosition( com::sun::star::table::SCellAddress _cell_address)
