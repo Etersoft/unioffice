76621587bd077c29ac4c0ebd1bf6b4338dbbfc94
Ivan Sinitsin <ivan@etersoft.ru>
2009-11-04 13:55:57 +0300

    Add full implemntation of getContent, setContent (XNamedRange)

--- OOWrappers/com/sun/star/sheet/x_named_range.cpp
+++ OOWrappers/com/sun/star/sheet/x_named_range.cpp
@@ -30,12 +30,64 @@ com::sun::star::sheet::XNamedRange::~XNamedRange( )
 
 BSTR com::sun::star::sheet::XNamedRange::getContent()
 {
- 	 TRACE_NOTIMPL;	 
+    TRACE_IN; 
+    HRESULT hr;
+    VARIANT res; 
+	BSTR ret_val = SysAllocString(L"");  
+    
+    VariantInit( &res );
+    
+    if ( IsNull() )
+    {
+        ERR( " m_pd_wrapper is NULL \n" );
+        return ( ret_val );     
+    }
+    
+    hr = AutoWrap (DISPATCH_METHOD, &res, m_pd_wrapper, L"getContent",0 );
+    if ( FAILED( hr ) )
+    {
+        ERR( " Call getContent \n" );
+    } else
+    {
+	  	SysFreeString( ret_val );  
+	  	ret_val = SysAllocString( V_BSTR( &res ) );
+    }
+ 
+    VariantClear( &res );
+ 
+    TRACE_OUT;
+    return ( ret_val );	 
 }
 
 HRESULT com::sun::star::sheet::XNamedRange::setContent( BSTR _content)
 {
- 	 TRACE_NOTIMPL;	 		
+    TRACE_IN; 
+    HRESULT hr;
+    VARIANT res, param1;    
+    
+    VariantInit( &res );
+    VariantInit( &param1 );
+    
+    if ( IsNull() )
+    {
+        ERR( " m_pd_wrapper is NULL \n" );
+        return ( E_FAIL );     
+    }
+    
+    V_VT( &param1 ) = VT_BSTR;
+    V_BSTR( &param1 ) = SysAllocString( _content );
+    
+    hr = AutoWrap (DISPATCH_METHOD, &res, m_pd_wrapper, L"setContent", 1, param1);
+    if ( FAILED( hr ) )
+    {
+        ERR( " Call setContent \n" );
+    }
+ 
+    VariantClear( &res );
+    VariantClear( &param1 );
+ 
+    TRACE_OUT;
+    return ( hr );	 		
 }
 			  
 com::sun::star::table::SCellAddress com::sun::star::sheet::XNamedRange::getReferencePosition()
