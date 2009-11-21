beff26ba5fb26f30ca6d2e865434402784863dd6
Ivan Sinitsin <ivan@etersoft.ru>
2009-11-21 12:02:56 +0300

    Add description of enum TableOrientation

---
+++ OOWrappers/com/sun/star/table/table_orientation.h
@@ -0,0 +1,44 @@
+/*
+ * header file - TableOrientation
+ *
+ * Copyright (C) 2009 Sinitsin Ivan (Etersoft) <ivan@etersoft.ru>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
+ */
+
+#ifndef __UNIOFFICE_TABLE_ORIENTATION_H__
+#define __UNIOFFICE_TABLE_ORIENTATION_H__
+
+namespace com
+{
+    namespace sun
+    {
+        namespace star
+        {
+		    namespace table 
+			{ 
+ 		  
+			enum TableOrientation
+			{				 
+				 COLUMNS,
+				 ROWS
+			};
+
+            } // namespace table
+        } // namespace star
+    } // namespace sun
+} // namespace com
+
+#endif // __UNIOFFICE_TABLE_ORIENTATION_H__
