842a17c459a38a62c3e76b5988bac9773cf206d5
Ivan Sinitsin <ivan@etersoft.ru>
2009-11-21 12:06:19 +0300

    Add description of enum ShadowLocation

---
+++ OOWrappers/com/sun/star/table/shadow_location.h
@@ -0,0 +1,47 @@
+/*
+ * header file - ShadowLocation
+ *
+ * Copyright (C) 2009 Sinitsin Ivan (Etersoft) <ivan@etersoft.ru>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
+ */
+
+#ifndef __UNIOFFICE_SHADOW_LOCATION_H__
+#define __UNIOFFICE_SHADOW_LOCATION_H__
+
+namespace com
+{
+    namespace sun
+    {
+        namespace star
+        {
+		    namespace table 
+			{ 
+ 		  
+			enum ShadowLocation
+			{				 
+				 NONE,
+				 TOP_LEFT,
+				 TOP_RIGHT,
+				 BOTTOM_LEFT,
+				 BOTTOM_RIGHT
+			};
+
+            } // namespace table
+        } // namespace star
+    } // namespace sun
+} // namespace com
+
+#endif // __UNIOFFICE_SHADOW_LOCATION_H__
