139c0626bd433d8d14dea8dd1f77690cc80237c9
Ivan Sinitsin <ivan@etersoft.ru>
2009-12-05 21:41:30 +0300

    Add implementation of getRangeAddress (XCellRAngeAddressable)

--- OOWrappers/com/sun/star/sheet/x_cell_range_addressable.cpp
+++ OOWrappers/com/sun/star/sheet/x_cell_range_addressable.cpp
@@ -32,5 +32,30 @@ com::sun::star::sheet::XCellRangeAddressable::~XCellRangeAddressable( )
 			  
 com::sun::star::table::CellRangeAddress  com::sun::star::sheet::XCellRangeAddressable::getRangeAddress()
 {
- 										 
+    TRACE_IN; 
+    HRESULT hr;
+    VARIANT res;
+	com::sun::star::table::CellRangeAddress ret_val;    
+    
+    VariantInit( &res );
+    
+    if ( IsNull() )
+    {
+        ERR( " m_pd_wrapper is NULL \n" );
+        return ( ret_val );     
+    }
+        
+    hr = AutoWrap (DISPATCH_METHOD, &res, m_pd_wrapper, L"getRangeAddress", 0);
+    if ( FAILED( hr ) )
+    {
+        ERR( " Call getRangeAddress \n" );
+    } else
+    {
+        ret_val.Init( V_DISPATCH( &res ) );      
+    }
+ 
+    VariantClear( &res );
+ 
+    TRACE_OUT;
+    return ( ret_val ); 										 
 } 
