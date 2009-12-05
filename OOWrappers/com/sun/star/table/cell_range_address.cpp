70d304c767021e87ffa0c954b042af10e7675d91
Ivan Sinitsin <ivan@etersoft.ru>
2009-12-05 20:19:17 +0300

    Add implementation of EndRow (CellRangeAddress)

--- OOWrappers/com/sun/star/table/cell_range_address.cpp
+++ OOWrappers/com/sun/star/table/cell_range_address.cpp
@@ -162,5 +162,33 @@ long com::sun::star::table::CellRangeAddress::EndColumn()
 
 long com::sun::star::table::CellRangeAddress::EndRow()
 {
- 	 
+    TRACE_IN;
+	VARIANT res;	 
+	HRESULT hr;
+	long ret_val = 0;
+		
+	if ( IsNull() )
+	{
+	   	ERR( " m_pd_wrapper is NULL \n" ); 
+	    return ( 0 );   	 
+    }	
+	
+	VariantInit( &res );
+		
+	hr = AutoWrap(DISPATCH_PROPERTYGET, &res, m_pd_wrapper, L"EndRow",0 );
+	if ( FAILED( hr ) )
+	{
+	    ERR( " failed EndRow \n" );   	 
+	    TRACE_OUT;
+	    return ( 0 );
+    } 
+	
+	VariantChangeTypeEx(&res, &res, 0, 0, VT_I4);
+	
+	ret_val = V_I4( &res );
+	
+	VariantClear( &res ); 
+	 
+    TRACE_OUT;
+	return ( ret_val );  	 
 }
