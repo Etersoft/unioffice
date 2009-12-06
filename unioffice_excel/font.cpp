3912ddfd747409d758dc50183dff24302e3c0175
Ivan Sinitsin <ivan@etersoft.ru>
2009-12-06 19:30:48 +0300

    Add implementation of Init() (CFont)

--- unioffice_excel/font.cpp
+++ unioffice_excel/font.cpp
@@ -345,8 +345,34 @@ HRESULT STDMETHODCALLTYPE CFont::Invoke(
             
 HRESULT CFont::Init( )
 {
-    TRACE_NOTIMPL;
-    return E_NOTIMPL;		
+     HRESULT hr = S_OK;   
+      
+     if (m_pITypeInfo == NULL)
+     {
+        ITypeLib* pITypeLib = NULL;
+        hr = LoadRegTypeLib(LIBID_Office,
+                                1, 0, // Номера версии
+                                0x00,
+                                &pITypeLib); 
+        
+       if (FAILED(hr))
+       {
+           ERR( " Typelib not register \n" );
+           return hr;
+       } 
+        
+       // Получить информацию типа для интерфейса объекта
+       hr = pITypeLib->GetTypeInfoOfGuid(IID__IFont, &m_pITypeInfo);
+       pITypeLib->Release();
+       if (FAILED(hr))
+       {
+          ERR(" GetTypeInfoOfGuid \n ");
+          return hr;
+       }
+       
+     }
+
+     return hr;		
 }
          
 HRESULT CFont::Put_Application( void* p_application)
