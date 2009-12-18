5dd8aa6274bb8327c0b4871d22b8a77c5aff63bd
Ivan Sinitsin <ivan@etersoft.ru>
2009-12-18 22:30:30 +0300

    Add new enum description - FontSlant

---
+++ OOWrappers/com/sun/star/awt/font_slant.h
@@ -0,0 +1,48 @@
+/*
+ * header file - FontSlant
+ *
+ * Copyright (C) 2009 Sinitsin Ivan (Etersoft) <ivan@etersoft.ru>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
+ */
+
+#ifndef __UNIOFFICE_FONT_SLANT_H__
+#define __UNIOFFICE_FONT_SLANT_H__
+
+namespace com
+{
+    namespace sun
+    {
+        namespace star
+        {
+		    namespace awt 
+			{ 
+ 		  
+			enum FontSlant
+			{				 
+				 NONE,
+				 OBLIQUE,
+				 ITALIC,
+				 DONTKNOW,
+				 REVERSE_OBLIQUE,
+				 REVERSE_ITALIC,
+			};
+
+            } // namespace table
+        } // namespace star
+    } // namespace sun
+} // namespace com
+
+#endif // __UNIOFFICE_FONT_SLANT_H__
