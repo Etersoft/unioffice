5416d6ce2c1c2a3fcd030b67bfadc4b413ad542f
Ivan Sinitsin <ivan@etersoft.ru>
2009-12-26 21:40:09 +0300

    Add implementation of put_Color (CFont)

--- unioffice_excel/font.cpp
+++ unioffice_excel/font.cpp
@@ -279,11 +279,35 @@ HRESULT STDMETHODCALLTYPE CFont::put_Bold(
     return E_NOTIMPL;  		
 }
         
-        /* [helpcontext][propput] */ HRESULT STDMETHODCALLTYPE CFont::put_Color( 
+HRESULT STDMETHODCALLTYPE CFont::put_Color( 
             /* [in] */ VARIANT RHS)
 {
-    TRACE_NOTIMPL;
-    return E_NOTIMPL;  		
+    TRACE_IN;
+    HRESULT hr;
+    long value = 0;
+    
+    CorrectArg(RHS, &RHS);
+    
+    hr = VariantChangeTypeEx(&RHS, &RHS, 0, 0, VT_I4);
+    if ( FAILED( hr ) )
+    {
+	    ERR( " VariantChangeTypeEx \n" );   
+		TRACE_OUT;
+		return ( E_FAIL );	 
+    }
+    
+	value = V_I4( &RHS );
+		
+	hr = m_oo_font.setCharColor( value );
+	if ( FAILED( hr ) )
+	{
+	    ERR( " m_oo_font.setCharColor \n" );  
+		TRACE_OUT;
+		return ( hr ); 	 
+    }			    
+    
+    TRACE_OUT;
+    return ( hr ); 		
 }
         
         /* [helpcontext][propget] */ HRESULT STDMETHODCALLTYPE CFont::get_ColorIndex( 
