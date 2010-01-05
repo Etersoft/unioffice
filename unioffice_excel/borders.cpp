be432524faf55cd2f12c38e312aecb7bfc07d331
Ivan Sinitsin <ivan@etersoft.ru>
2010-01-05 17:24:07 +0300

    Add implementation of Put_Application, Put_Parent (CBorders)

--- unioffice_excel/borders.cpp
+++ unioffice_excel/borders.cpp
@@ -265,14 +265,22 @@ HRESULT CBorders::Init( )
        
 HRESULT CBorders::Put_Application( void* p_application)
 {
-    TRACE_NOTIMPL;
-	return E_NOTIMPL; 		
+    TRACE_IN;
+    
+    m_p_application = p_application;
+    
+    TRACE_OUT;    
+    return S_OK; 		
 } 
 
 HRESULT CBorders::Put_Parent( void* p_parent)
 {
-    TRACE_NOTIMPL;
-	return E_NOTIMPL; 		
+   TRACE_IN;  
+      
+   m_p_parent = p_parent;       
+      
+   TRACE_OUT;
+   return S_OK;  		
 }            
      
 	        
