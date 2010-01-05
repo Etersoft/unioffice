64e38321d9f87756ffc8b1e0a1a31e6199226585
Ivan Sinitsin <ivan@etersoft.ru>
2010-01-05 19:53:44 +0300

    Add new wrapper for struct Size

---
+++ OOWrappers/com/sun/star/awt/size.cpp
@@ -0,0 +1,75 @@
+/*
+ * source file - Size
+ *
+ * Copyright (C) 2009 Sinitsin Ivan (Etersoft) <ivan@etersoft.ru>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
+ */
+
+#include "size.h"
+   
+com::sun::star::awt::Size::Size( ):com::sun::star::uno::XBase()
+{
+ 																                                                                        
+}
+
+com::sun::star::awt::Size::~Size( )
+{   
+	           							
+} 
+
+com::sun::star::awt::Size& com::sun::star::awt::Size::operator=( const com::sun::star::awt::Size &obj)
+{
+   if ( this == &obj )
+   {
+       return ( *this );                 
+   }    
+   
+   if ( m_pd_wrapper != NULL )
+   {
+       m_pd_wrapper->Release();
+       m_pd_wrapper = NULL;        
+   } 
+
+   m_pd_wrapper = obj.m_pd_wrapper;
+   if ( m_pd_wrapper != NULL )
+       m_pd_wrapper->AddRef();
+   
+   return ( *this );  		 
+}
+
+HRESULT setWidth( long value)
+{
+    TRACE_NOTIMPL;
+	return E_NOTIMPL;  		
+}
+
+HRESULT getWidth( long& value)
+{
+    TRACE_NOTIMPL;
+	return E_NOTIMPL;  		
+}
+				
+HRESULT setHeight( long value)
+{
+    TRACE_NOTIMPL;
+	return E_NOTIMPL;  		
+}
+
+HRESULT getHeight( long& value)
+{
+    TRACE_NOTIMPL;
+	return E_NOTIMPL;  		
+}
---
+++ OOWrappers/com/sun/star/awt/size.h
@@ -0,0 +1,57 @@
+/*
+ * header file - Size
+ *
+ * Copyright (C) 2009 Sinitsin Ivan (Etersoft) <ivan@etersoft.ru>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
+ */
+
+#ifndef __UNIOFFICE_SIZE_H__
+#define __UNIOFFICE_SIZE_H__
+
+#include "../uno/x_base.h"
+
+namespace com
+{
+    namespace sun
+    {
+        namespace star
+        {
+		    namespace awt 
+			{ 
+ 		  
+			class Size:
+   				  public virtual com::sun::star::uno::XBase 
+			{
+			public:
+				   				 
+		        Size( );
+			    virtual ~Size( ); 
+				
+				Size& operator=( const Size &obj); 
+				
+         	  	HRESULT setWidth( long );
+         	  	HRESULT getWidth( long& );			  	
+				
+         	  	HRESULT setHeight( long );
+         	  	HRESULT getHeight( long& ); 				
+			};
+
+            } // namespace table
+        } // namespace star
+    } // namespace sun
+} // namespace com
+
+#endif // __UNIOFFICE_SIZE_H__
