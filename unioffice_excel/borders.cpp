866db68ce6d70160a4a75b1fcc648d4417131d50
Ivan Sinitsin <ivan@etersoft.ru>
2010-01-05 18:04:10 +0300

    Add implementation of get_Application (CBorders)

--- unioffice_excel/borders.cpp
+++ unioffice_excel/borders.cpp
@@ -163,12 +163,25 @@ HRESULT STDMETHODCALLTYPE CBorders::Invoke(
     return ( hr ); 		
 } 
 
-			   // IBorders              
-        /* [helpcontext][propget] */ HRESULT STDMETHODCALLTYPE CBorders::get_Application( 
+  		   // IBorders              
+HRESULT STDMETHODCALLTYPE CBorders::get_Application( 
             /* [retval][out] */ Application	**RHS)
 {
-    TRACE_NOTIMPL;
-	return E_NOTIMPL; 		
+   TRACE_IN;             
+   
+   if ( m_p_application == NULL )
+   {
+       ERR( " m_p_application == NULL \n " ); 
+       TRACE_OUT;
+       return ( S_FALSE );    
+   }
+            
+   HRESULT hr = S_OK;
+   
+   hr = (static_cast<Application*>( m_p_application ))->get_Application( RHS );          
+             
+   TRACE_OUT;
+   return hr; 		
 } 
         
         /* [helpcontext][propget] */ HRESULT STDMETHODCALLTYPE CBorders::get_Creator( 
